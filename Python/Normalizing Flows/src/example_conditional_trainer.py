c6a88b210d791b4a40208aac98556c32996bac2d
Volalex2000 <volkov.ad@phystech.edu>
2024-02-06 21:59:04 +0100

    Add dividing the set on train and test

--- Python/Normalizing Flows/src/example_conditional_trainer.py
+++ Python/Normalizing Flows/src/example_conditional_trainer.py
@@ -7,15 +7,22 @@ from train import ConditionalTrainer
 
 import matplotlib.pyplot as plt
 
+
 flow = ConditionalNF(50, 3, 10)
 optimizer = torch.optim.Adam(flow.parameters(), lr=5e-4)
 data = ZeeDataset('../data/data_dict.pkl')
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
+### Devising the data on train and test
+train_len = int(0.8 * len(data))
+data_train, data_test = torch.utils.data.random_split(data, 
+                                                      [train_len, len(data)-train_len])
+
 nb_epochs = 30
 batch_size = 1000
 
-dataloader = torch.utils.data.DataLoader(data, batch_size=batch_size, shuffle=False)
+# question: why you set shuffle=False???
+dataloader = torch.utils.data.DataLoader(data_train, batch_size=batch_size, shuffle=True)
 
 trainer = ConditionalTrainer(flow, optimizer, dataloader, 3, 10, 1e-2, device)
 
