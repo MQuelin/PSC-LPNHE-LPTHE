4d86e927c6573e1f6d30671a1e2f38593f064b3c
MQuelin <mq.no.spam@orange.fr>
2024-02-07 15:53:48 +0100

    Trained New Models

--- Python/Normalizing Flows/src/example_conditional_trainer.py
+++ Python/Normalizing Flows/src/example_conditional_trainer.py
@@ -7,7 +7,7 @@ from train import ConditionalTrainer
 
 import matplotlib.pyplot as plt
 
-flow = ConditionalNF(50, 3, 10)
+flow = ConditionalNF(12, 3, 10)
 optimizer = torch.optim.Adam(flow.parameters(), lr=5e-4)
 data = ZeeDataset('../data/data_dict.pkl')
 device = 'cuda'
@@ -21,9 +21,7 @@ trainer = ConditionalTrainer(flow, optimizer, dataloader, 3, 10, 1e-2, device)
 
 loss = trainer.train(nb_epochs)
 
-trainer.save_at(save_path= "../models", save_name="ConditionalNF_50layers_10kZee_noRings_310124.pt")
+trainer.save_at(save_path= "../models", save_name="ConditionalNF_12layers_10kZee_noRings_310124.pt")
 
 plt.plot(range(len(loss)), loss)
 plt.show()
-
-print(loss)
