a69210ae33504e28c3bd9fee3fe0aacf746847af
Limana <alexandre.violleau@laposte.net>
2024-04-24 14:58:56 +0200

    Add a way to generate 2D gaussian distrib

---
+++ Python/Normalizing Flows/src/gaussian_generator.py
@@ -0,0 +1,54 @@
+#%%
+import numpy as np
+import matplotlib.pyplot as plt
+import random
+
+#%%
+
+mean = (0, 0)
+cov = [[1,0],[0,1]]
+
+mu = 0
+sigma = 1
+mu_Y = .3
+sigma_Y = 1.7
+
+Matransfo = np.random.exponential(size=(2,2))
+
+#%%
+
+def gauss_2d(mu, sigma):
+    x = random.gauss(mu, sigma)
+    y = random.gauss(mu, sigma)
+    return (x, y)
+
+def generation(N=10**4):
+    X = [random.gauss(mu, sigma) for i in range(N)]
+    Y = [random.gauss(mu_Y, sigma_Y) for i in range(N)]
+    X = np.array(X).transpose()
+    Y = np.array(Y).transpose()
+    return np.vstack((X, Y))
+
+def transfo(L):
+    n = L.shape[1]
+    ret = []
+    for i in range(n):
+        ret.append(Matransfo@L[:, i])
+    return np.array(ret).transpose()
+
+#%%
+
+L = generation()
+print(L.shape)
+
+R = transfo(L)
+print(R.shape)
+#%%
+
+plt.plot(L[0,:], L[1,:], 'x')
+plt.plot(R[0,:], R[1,:], 'o')
+plt.axis('equal')
+plt.show()
+
+
+#%%
